01bd4ff9f67bb3fe6e1acf8613831e5871f197cf
= <jord52@gmail.com>
2014-08-16 17:08:50 -0400

    deleted some stuff

--- StiGame/ControllerAxisEventListener.cpp
+++
@@ -1,8 +0,0 @@
-#include "ControllerAxisEventListener.h"
-
-namespace StiGame
-{
-	ControllerAxisEventListener::ControllerAxisEventListener() {}
-	ControllerAxisEventListener::~ControllerAxisEventListener() {}
-	void ControllerAxisEventListener::handleEvent(ControllerAxisEventThrower *src, ControllerAxisEventArgs *args) {}
-}
